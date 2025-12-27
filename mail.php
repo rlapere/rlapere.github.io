777753ebf1197992609d685bf4d9adaf12add215
rlapere <94524458+rlapere@users.noreply.github.com>
2025-12-27 10:30:07 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,15 +1,1 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <title>Envoi d'un message par formulaire</title>
-</head>
 
-<body>
-    <?php
-    $retour = mail('remy.lapere.ensta@gmail.com', 'Contact Shiatsu', $_POST['message'], '');
-    if ($retour)
-        echo '<p>Votre message a bien été envoyé.</p>';
-    ?>
-</body>
-</html>
